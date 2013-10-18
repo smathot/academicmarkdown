ead5a8da15770e726d19722aa521d5e50df55ebe
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-10-18 18:01:36 +0200

    Add git module

---
+++ academicmarkdown/git.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of academicmarkdown.
+
+academicmarkdown is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+academicmarkdown is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with academicmarkdown.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from academicmarkdown import build
+from subprocess import check_output, call
+import os
+import shlex
+
+exportFolder = u'export'
+exportFormats = u'odt', u'pdf', u'docx'
+
+def snapshot(src, msg=u'snapshot'):
+	
+	"""
+	Commits the current state of the repository and exports a snapshot of the
+	current documents.
+	
+	Arguments:
+	src		--	The source Markdown document.
+	
+	Keyword arguments:
+	msg		--	A commit message. (default=u'snapshot')
+	"""
+	
+	cmd = [u'git', u'commit', u'-am', msg]
+	print u'Committing (msg: %s)' % msg
+	call(cmd)
+	cmd = [u'git', u'log', u'--pretty=format:[%cd #%h] %s', u'--date=iso', \
+		u'-1']
+	tag = check_output(cmd)
+	folder = os.path.join(exportFolder, tag)
+	print u'Exporting to %s' % folder
+	if os.path.exists(folder):
+		raise Exception( \
+			u'Folder %s already exists! There is probably nothing new to export.' \
+			% folder)
+	os.mkdir(folder)
+	if u'pdf' in exportFormats:
+		build.PDF(src, os.path.join(folder, u'export.pdf'))
+	if u'docx' in exportFormats:
+		build.DOCX(src, os.path.join(folder, u'export.docx'))
+	if u'odt' in exportFormats:
+		build.ODT(src, os.path.join(folder, u'export.odt'))
+	if u'html' in exportFormats:
+		build.HTML(src, os.path.join(folder, u'export.html'))
